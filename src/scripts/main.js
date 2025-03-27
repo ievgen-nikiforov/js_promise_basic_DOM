5ed91f9b03d7663b5b955729b958b898018deeee
inikiforov <ievgen.nikiforov@tedata.de>
2025-03-27 12:28:14 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
-'use strict';
+const logo = document.getElementsByClassName('logo')[0];
 
-// write your code here
+logo.addEventListener('click', function () {
+  const promise1 = new Promise((resolve, reject) => {
+    resolve();
+  });
+
+  promise1.then(successHandler).catch(errorHandler);
+});
+
+setTimeout(() => {
+  const promise2 = new Promise((resolve, reject) => {
+    reject();
+  });
+
+  promise2.then(successHandler).catch(errorHandler);
+}, 3000);
+
+function successHandler() {
+  const divEl = document.createElement('div');
+
+  divEl.className = 'message';
+  divEl.innerText = 'Promise was resolved!';
+  document.querySelector('body').append(divEl);
+}
+
+function errorHandler() {
+  const divEl = document.createElement('div');
+
+  divEl.className = 'message error-message';
+  divEl.innerText = 'Promise was rejected!';
+  document.querySelector('body').append(divEl);
+}
